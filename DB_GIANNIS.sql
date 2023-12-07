dc0fb0cd8fb54b4c2a98b4e309c188e98b20a147
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2023-12-07 19:34:23 +0200

    Update DB_GIANNIS.sql

--- DB_GIANNIS.sql
+++ DB_GIANNIS.sql
@@ -1,31 +1,38 @@
 DROP DATABASE IF EXISTS project_giannis;
 CREATE DATABASE project_giannis;
+
 /* table creations of our database */
 /*λάθος εδώ*/
-CREATE TABLE an8rwpino_dinamiko(
+
+CREATE TABLE anthrwpino_dinamiko(
 	username VARCHAR(30) not null,
     password VARCHAR(30) not null,
     PRIMARY KEY(telephone)
 );
 /*είπε στην διάλεξη πως κάθε διασώστης έχει ένα ΜΟΝΟ όχημα άρα πρακτικά ο διασώστης και το όχημα ταυτιζονταί,για το φορτίο δεν έχω ιδέα θα εξηγήσω σε call*/
+
 CREATE TABLE oxhma/diaswsths(
 username_oxhmatos/diaswsth varchar(30) not null,
-topo8esia_oxhmatos not null,
+topothesia_oxhmatos not null,
 fortio varchar(30),
 katastash enum()
 );
+
 CREATE TABLE admin(
 username_admin varchar(30) not null,
 password_admin varchar(30) not null
 );
+
 CREATE TABLE poliths(
 poliths_username varchar(30),
 onoma varchar(30) not null,
 eponymo varchar(30) not null,
 telephone int(10) not null,
-topo8esia_polith(30),
+topothesia_polith(30),
 );
-/*etimo_8ewrhtika*/
+
+/*etimo_thewrhtika*/
+
 create table wants(
 poliths_username_who_wants varchar(30) not null,
 wanted_eidos varchar(30) not null,
@@ -33,11 +40,12 @@ primary key(poliths_username_who_wants,wanted_eidos),
 constraint poliths_Wants
 	foreign key(poliths_username_who_wants) references poliths(poliths_username)
 	ON DELETE CASCADE ON UPDATE CASCADE,
-constraint aga8o_wanted
-	foreign key(wanted_eidos) references aga8a(eidos)
+constraint agatho_wanted
+	foreign key(wanted_eidos) references agatha(eidos)
 	ON DELETE CASCADE ON UPDATE CASCADE
 );
-/*etimo_8ewrhtika*/
+
+/*etimo_thewrhtika*/
 create table gives(
 poliths_username_who_gives varchar(30) not null,
 given_eidos varchar(30) not null,
@@ -45,24 +53,25 @@ primary key(poliths_username_who_gives,given_eidos),
 constraint poliths_Gives
 foreign key(poliths_username_who_gives) references poliths(poliths_username)
 ON DELETE CASCADE ON UPDATE CASCADE,
-constraint aga8o_given
-foreign key(given_eidos) references aga8a(eidos)
+constraint agatho_given
+foreign key(given_eidos) references agatha(eidos)
  ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE aga8a(
+CREATE TABLE agatha(
 kathgoria varchar(30),
 eidos varchar(30) not null,
-ari8mos int not null,
+arithmos int not null,
 perigraphi varchar(60) default'no description',
 primary key(eidos)
 
 );
+
 CREATE TABLE task(
-topo8esia_task,
+topothesia_task,
 
 
-CONSTRAINT Topo8esia_polith
-    FOREIGN KEY(topo8esia_task) REFERENCES polith(topo8esia_polith)
+CONSTRAINT Topothesia_polith
+    FOREIGN KEY(topothesia_task) REFERENCES polith(topothesia_polith)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
